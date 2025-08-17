fcbc36813f5564385bdd227077f9869062d0a78c
joulie <ajoulie@live.fr>
2025-08-17 13:02:47 +0200

    ajout d'une page d'install

--- angular/src/app/components/navigation/menu/underMenu/mes-bdd/mes-bdd-donnees/mes-bdd-donnees.ts
+++ angular/src/app/components/navigation/menu/underMenu/mes-bdd/mes-bdd-donnees/mes-bdd-donnees.ts
@@ -4,8 +4,22 @@ import { Component } from '@angular/core';
   selector: 'app-mes-bdd-donnees',
   imports: [],
   templateUrl: './mes-bdd-donnees.html',
-  styleUrl: './mes-bdd-donnees.css'
+  styleUrl: './mes-bdd-donnees.css',
+  standalone: true
 })
 export class MesBddDonnees {
-
+  copyToClipboard(element: HTMLElement) {
+    const text = element.innerText;
+    if (navigator && navigator.clipboard) {
+      navigator.clipboard.writeText(text);
+    } else {
+      // fallback pour vieux navigateurs
+      const textarea = document.createElement('textarea');
+      textarea.value = text;
+      document.body.appendChild(textarea);
+      textarea.select();
+      document.execCommand('copy');
+      document.body.removeChild(textarea);
+    }
+  }
 }
