87e1634177edfc1b702f84caeb1f5ed675f7fb0f
ajoulie <alexandre.joulie@capgemini.com>
2025-08-26 15:43:27 +0200

    affichage d'une pop in dans gateaux

--- angular/src/app/components/gateaux/gateaux.ts
+++ angular/src/app/components/gateaux/gateaux.ts
@@ -10,18 +10,30 @@ import { CommonModule } from '@angular/common';
 	styleUrls: ['./gateaux.css']
 })
 export class Gateaux {
-		patisseries = [
-			{ nom: 'Éclair au chocolat', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Tarte aux fraises', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Paris-Brest', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Opéra', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Mille-feuille', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Religieuse', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Saint-Honoré', image: 'assets/images/leaf_rake.png' },
-			{ nom: 'Tarte citron', image: 'assets/images/leaf_rake.png' }
-		];
+	patisseries = [
+		{ nom: 'Éclair au chocolat', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Tarte aux fraises', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Paris-Brest', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Opéra', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Mille-feuille', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Religieuse', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Saint-Honoré', image: 'assets/images/leaf_rake.png' },
+		{ nom: 'Tarte citron', image: 'assets/images/leaf_rake.png' }
+	];
+
+	popinVisible = false;
+	popinGateau: { nom: string, image: string } | null = null;
 
 	selectionner(nom: string) {
-		alert('Vous avez sélectionné : ' + nom);
+		const gateau = this.patisseries.find(p => p.nom === nom);
+		if (gateau) {
+			this.popinGateau = gateau;
+			this.popinVisible = true;
+		}
+	}
+
+	fermerPopin() {
+		this.popinVisible = false;
+		this.popinGateau = null;
 	}
 }
